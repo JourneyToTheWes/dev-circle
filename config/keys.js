c3f1e8eebfbfb7b58227f99d667e739cb94e6b01
Wesley Wei <wesley631w@gmail.com>
2019-04-04 16:20:18 -0700

    Initial express server setup and route files for the api

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+// module.exports -> making a way for the objects available outside of this scope
+module.exports = {
+    mongoURI: "mongodb://wesley:devcircle1@ds227243.mlab.com:27243/devcircle"
+};
