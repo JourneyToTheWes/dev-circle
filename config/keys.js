bf65d507e6bb5ac6694b7dee04ce0d7b3b701714
Wesley Wei <wesley631w@gmail.com>
2019-04-05 11:57:44 -0700

    Implemented login api route.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 // module.exports -> making a way for the objects available outside of this scope
 module.exports = {
-    mongoURI: "mongodb://wesley:devcircle1@ds227243.mlab.com:27243/devcircle"
+    mongoURI: "mongodb://wesley:devcircle1@ds227243.mlab.com:27243/devcircle",
+    secretOrKey: "secret"
 };
