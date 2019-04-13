0049bff67b939397c83c0497627bd933d6fa5784
Wesley Wei <wesley631w@gmail.com>
2019-04-13 00:44:59 -0700

    Removed some code and modified get profile route.

--- validation/profile.js
+++ validation/profile.js
@@ -37,45 +37,11 @@ module.exports = function validateProfileInput(data) {
 
     websites.forEach(site => {
         if (!isEmpty(data[site])) {
-            errors[site] = 'Not a valid URL';
+            if (!validator.isURL(data[site])) {
+                errors[site] = 'Not a valid URL';
+            }
         }
     });
-
-    // if (!isEmpty(data.website)) {
-    //     if (!validator.isURL(data.website)) {
-    //         errors.website = 'Not a valid URL';
-    //     }
-    // }
-
-    // if (!isEmpty(data.youtube)) {
-    //     if (!validator.isURL(data.youtube)) {
-    //         errors.youtube = 'Not a valid URL';
-    //     }
-    // }
-
-    // if (!isEmpty(data.twitter)) {
-    //     if (!validator.isURL(data.twitter)) {
-    //         errors.twitter = 'Not a valid URL';
-    //     }
-    // }
-
-    // if (!isEmpty(data.facebook)) {
-    //     if (!validator.isURL(data.facebook)) {
-    //         errors.facebook = 'Not a valid URL';
-    //     }
-    // }
-
-    // if (!isEmpty(data.linkedin)) {
-    //     if (!validator.isURL(data.linkedin)) {
-    //         errors.linkedin = 'Not a valid URL';
-    //     }
-    // }
-
-    // if (!isEmpty(data.instagram)) {
-    //     if (!validator.isURL(data.instagram)) {
-    //         errors.instagram = 'Not a valid URL';
-    //     }
-    // }
 
     return {
         errors,
