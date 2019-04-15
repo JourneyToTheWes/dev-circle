af17c847021f9390c4a39a9380fd2b278ae9cdac
Wesley Wei <wesley631w@gmail.com>
2019-04-15 02:37:39 -0700

    Added routes for getting post/posts and deleting a post.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,6 +15,27 @@ const validatePostInput = require('../../validation/post');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
+// @route   GET api/posts/
+// @desc    Get posts
+// @access  Public
+router.get('/', (req, res) => {
+    Post.find()
+        .sort({ date: -1 }) // Sort 'date' descending
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json({ nopostfound: 'No posts found' }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err =>
+            res.status(404).json({ nopostfound: 'No post found with that ID' })
+        );
+});
+
 // @route   POST api/posts/
 // @desc    Create post
 // @access  Private
@@ -39,6 +60,29 @@ router.post(
         });
 
         newPost.save().then(post => res.json(post));
+    }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Get post
+// @access  Private
+router.delete(
+    '/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Post.findOneAndRemove({ _id: req.params.id, user: req.user.id })
+            .then(post => {
+                // If no post then user is not authorized to delete
+                // otherwise, the post is deleted
+                return !post
+                    ? res
+                          .status(404)
+                          .json({ notauthorized: 'User not authorized' })
+                    : res.status(200).json({ success: 'Post deleted' });
+            })
+            .catch(err =>
+                res.status(404).json({ postnotfound: 'No post found' })
+            );
     }
 );
 
