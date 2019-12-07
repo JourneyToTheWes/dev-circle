ef0f4335c072c99bf31fc01e65253d09186ef63d
Wesley Wei <weswei@u.washington.edu>
2019-12-06 23:52:21 -0800

    Updating backend with new course refactors - Using async/await - Adding github to profile routes

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-// module.exports -> making a way for the objects available outside of this scope
-module.exports = {
-    mongoURI: "mongodb://wesley:devcircle1@ds227243.mlab.com:27243/devcircle",
-    secretOrKey: "secret"
-};
