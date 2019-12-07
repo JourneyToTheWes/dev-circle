177f8de221dcd1ce2cd9df8705a8ddf5f50a96b6
Wesley Wei <weswei@u.washington.edu>
2019-12-07 11:24:31 -0800

    Added login action and auth token logic for redirection.

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import jwt_decode from 'jwt-decode';
-// import setAuthToken from './utils/setAuthToken';
-// import { setCurrentUser, logoutUser } from './actions/authActions';
-// import { clearCurrentProfile } from './actions/profileActions';
 
-// Redux
-import { Provider } from 'react-redux';
-import store from './store';
-
+// Components
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 
+// Util
+import setAuthToken from './utils/setAuthToken';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/authActions';
+
 import './App.css';
 
-// Check for token
-// if (localStorage.jwtToken) {
-//     // Set auth token header auth
-//     setAuthToken(localStorage.jwtToken);
-//     // Decode token and get user info and exp
-//     const decoded = jwt_decode(localStorage.jwtToken);
-//     // Set user and isAuthenticated
-//     store.dispatch(setCurrentUser(decoded));
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
+}
 
-//     // Check for expired token
-//     const currentTime = Date.now() / 1000;
-//     if (decoded.exp < currentTime) {
-//         // Logout user
-//         store.dispatch(logoutUser());
-//         // Clear current Profile
-//         store.dispatch(clearCurrentProfile());
-//         // Redirect to login
-//         window.location.href = '/login';
-//     }
-// }
+const App = () => {
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, []);
 
-const App = () => (
-    <Provider store={store}>
-        <Router>
-            <Fragment>
-                <Navbar />
-                <Route exact path='/' component={Landing} />
-                <section className='container'>
-                    <Alert />
-                    <Switch>
-                        <Route exact path='/login' component={Login} />
-                        <Route exact path='/register' component={Register} />
-                    </Switch>
-                </section>
-            </Fragment>
-        </Router>
-    </Provider>
-);
+    return (
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path='/' component={Landing} />
+                    <section className='container'>
+                        <Alert />
+                        <Switch>
+                            <Route exact path='/login' component={Login} />
+                            <Route exact path='/register' component={Register} />
+                        </Switch>
+                    </section>
+                </Fragment>
+            </Router>
+        </Provider>
+    )
+};
 
 export default App;
