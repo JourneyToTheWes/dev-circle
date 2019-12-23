411bb66e1744470b6defcb80f6508ea433e3c93b
Wesley Wei <weswei@u.washington.edu>
2019-12-23 14:07:23 -0800

    Added profile Github display for users.

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -33,7 +33,7 @@ const ProfileEducation = ({
     </div>;
 
 ProfileEducation.propTypes = {
-    education: PropTypes.array.isRequired
+    education: PropTypes.object.isRequired
 }
 
 export default ProfileEducation;
