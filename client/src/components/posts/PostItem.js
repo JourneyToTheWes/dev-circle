bb10617b20ffabb301de049b8031eb47650217ee
Wesley Wei <weswei@u.washington.edu>
2019-12-24 10:43:52 -0800

    Added actions for like an remove like. Added logic to handle the posts reducer.

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/postActions';
 
 const PostItem = ({
+    addLike,
+    removeLike,
     auth,
     post: {
         _id,
@@ -34,7 +37,7 @@ const PostItem = ({
             <p class="post-date">
                 Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
-            <button type="button" class="btn btn-light">
+            <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
                 <i class="fas fa-thumbs-up"></i>{' '}
                 {
                     likes.length > 0 && (
@@ -42,7 +45,7 @@ const PostItem = ({
                     )
                 }
             </button>
-            <button type="button" class="btn btn-light">
+            <button onClick={e => removeLike(_id)} type="button" class="btn btn-light">
                 <i class="fas fa-thumbs-down"></i>
             </button>
             <Link to={`/post/${_id}`} class="btn btn-primary">
@@ -76,4 +79,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
